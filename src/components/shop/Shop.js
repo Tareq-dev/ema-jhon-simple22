7f2dff58af662716d60e62fea5e1619fd5634d25
Tarequl Islam <tareque.dev@gmail.com>
2022-04-24 12:45:15 +0600

    server setup with review error

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -8,10 +8,18 @@ import "./Shop.css";
 import { useEffect, useState } from "react";
 
 const Shop = () => {
-  const [products] = useProducts();
+  const [products, setProducts] = useState([]);
   const [cart, setCart] = useCart(products);
   const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [page,size]);
 
   useEffect(() => {
     fetch("http://localhost:5000/productCount")
@@ -48,10 +56,21 @@ const Shop = () => {
           />
         ))}
 
-        <div>
+        <div className="pagination mt-5 mb-5">
           {[...Array(pageCount).keys()].map((number) => (
-            <button className="border px-2 py-1 mx-2 bg-white">{number + 1}</button>
+            <button
+              onClick={() => setPage(number)}
+              className={page === number ? "selectedPage" : ""}
+            >
+              {number + 1}
+            </button>
           ))}
+          <select onChange={(e)=>setSize(e.target.value)}>
+          <option value="5">5</option>
+          <option value="10" selected>10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+          </select>
         </div>
       </div>
       <div className="order-container">
